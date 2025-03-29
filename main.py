ce2a320b86d73c1f51de386d7da152e01518d6fa
simondrauz <s.drauz@gmail.com>
2025-03-29 22:28:45 +0100

    (refactor) comment out modelling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from data_modelling import preprocess_data, train_model, load_config
-from data_cleaning import clean_data
+from data_cleaning import clean_data, display_years
 import pandas as pd
+import shap
 
 
 # Load dataset
@@ -13,30 +14,43 @@ preproc_cfg = config["preprocess"]
 training_cfg = config["training"]
 
 # Clean the data using configuration parameters
-columns_for_model = [preproc_cfg["target"]] + preproc_cfg["numerical_cols"] + preproc_cfg["categorical_cols"]
+columns_for_model = [preproc_cfg["target"]] + preproc_cfg["numerical_cols"] + preproc_cfg["categorical_cols"] + preproc_cfg["temporal_cols"] + preproc_cfg["spatial_cols"]
 df = clean_data(df, columns_for_model)
 
-# Preprocess the data using configuration parameters
-X, y = preprocess_data(
-    df,
-    preproc_cfg["target"],
-    preproc_cfg["numerical_cols"],
-    preproc_cfg["categorical_cols"],
-    preproc_cfg.get("grid_col"),
-    preproc_cfg.get("id_col")
-)
+# Change year cols
+df_years = display_years(df)
 
-# Train the model using configuration parameters
-model, metrics, feature_importances = train_model(
-    X, y,
-    test_size=training_cfg["test_size"],
-    random_state=training_cfg["random_state"],
-    params=training_cfg.get("params"),
-    num_boost_round=training_cfg["num_boost_round"],
-    early_stopping_rounds=training_cfg["early_stopping_rounds"],
-    seed=training_cfg.get("seed")
-)
+# Export the cleaned DataFrame to a CSV file
+df_years.to_csv("Data/HiDrive/panel/CampusFile_WM_cities_cleaned.csv", index=False)
 
-# Display feature importances
-print("\nFeature Importances:")
-print(feature_importances)
+# # Preprocess the data using configuration parameters
+# X, y = preprocess_data(
+#    df,
+#    preproc_cfg["target"],
+#    preproc_cfg["numerical_cols"],
+#    preproc_cfg["categorical_cols"],
+#    preproc_cfg.get("grid_col"),
+#    preproc_cfg.get("id_col")
+#)
+#
+## Train the model using configuration parameters
+#model, metrics, feature_importances = train_model(
+#    X, y,
+#    test_size=training_cfg["test_size"],
+#    random_state=training_cfg["random_state"],
+#    params=training_cfg.get("params"),
+#    num_boost_round=training_cfg["num_boost_round"],
+#    early_stopping_rounds=training_cfg["early_stopping_rounds"],
+#    seed=training_cfg.get("seed")
+#)
+#
+## Display feature importances
+#print("\nFeature Importances:")
+#print(feature_importances)
+#
+## Assuming your trained LightGBM model is stored in the variable `model`
+#explainer = shap.TreeExplainer(model)
+#shap_values = explainer.shap_values(X)
+#
+## Plot summary plot
+#shap.summary_plot(shap_values, X)
